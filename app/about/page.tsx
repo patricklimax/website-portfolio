a54acab765b64f632267a93538d564020d52ec4d
patricklimax <patricklimax@outlook.com.br>
2024-08-01 19:01:48 -0300

    feat: creating application pages and routes

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return ( 
+    <section
+      className='mx-auto max-w-5xl grid grid-cols-1 gap-4 md:grid md:grid-cols-12 md:grid-rows-12 md:h-[696px]'>
+      About Page
+    </section>
+   );
+}
+ 
+export default AboutPage;
---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+const ProjectsPage = () => {
+  return (
+    <section
+      className='mx-auto max-w-5xl grid grid-cols-1 gap-4 md:grid md:grid-cols-12 md:grid-rows-12 md:h-[696px]'>
+      Project Page
+    </section>  );
+}
+
+export default ProjectsPage;
